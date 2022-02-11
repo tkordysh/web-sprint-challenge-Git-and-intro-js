af2566c85744cd22121a3a8668022b10c53171b0
Tony Kordysh <tkordysh@gmail.com>
2022-02-11 16:00:52 -0600

    solved task 9. First sprint done!

--- index.js
+++ index.js
@@ -353,11 +353,17 @@ Use artistByCountry to do the following:
 For example artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, nationality){
+  const nationArray = [];
+  for (let i = 0; i < array.length; i++) {
+     if (array[i].nationality === nationality) {
+       nationArray.push(array[i].name)
+     }
+  }
+  return nationArray;
 }
 
-
+console.log('task 9', artistByCountry(artists, 'Spanish'));
 
 /* ***** END OF TASKS ***** */
 
